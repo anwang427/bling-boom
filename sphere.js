e319f4760da71b411ecccd049b610c96b6b7e177
Anna Wang <anna.wang2019@gmail.com>
2020-03-24 10:53:47 -0400

    Update sphere.js
    Updated motion trail

--- sphere.js
+++ sphere.js
@@ -1,3 +1,7 @@
+//https://www.developphp.com/video/JavaScript/Particle-Effect-System-Tutorial-Snow-Falling-Animation 
+// put the ^ horizontal as little pinpoints of light for randomized star animations!
+//mysic by Mark Maxwell - Sacred Spheres
+
 var canvas = document.querySelector("#myCanvas");
 canvas.width = window.innerWidth; //allow the circle to traverse the entire screen
 canvas.height = window.innerHeight;
@@ -6,7 +10,7 @@ var context = canvas.getContext("2d");
 var mouseX = 0;
 var mouseY = 0;
  
-canvas.addEventListener("mousemove", setMousePosition, false);
+canvas.addEventListener("mousemove", setMousePosition, false); //set the program to 'listen' to mouse movement
  
 function setMousePosition(e) {
   mouseX = e.clientX 
@@ -19,23 +23,65 @@ function update() {
 	for (var i = 0; i < positions.length; i++) {
         var ratio = (i + 1) / positions.length;
 
-        context.beginPath();
-        context.arc(positions[i].x, positions[i].y, 15, 0, 2 * Math.PI, true);
-        context.fillStyle = "rgba(204, 102, 153, " + ratio / 2 + ")";
-        context.fill();
-    }
+		context.beginPath();
+		
+		//the trailing path behind the comet
+		context.arc(positions[i].x + 60, positions[i].y, ratio, 0, 2 * Math.PI, true);
+		context.fillStyle = "rgba(255, 255, 255, " + ratio / 2 + ")";
+		context.fill();
+
+		context.arc(positions[i].x + 60, positions[i].y - 5, ratio / 1.2, 0, 2 * Math.PI, true);
+		context.arc(positions[i].x + 60, positions[i].y + 5, ratio / 1.2, 0, 2 * Math.PI, true);
+		context.fillStyle = "rgba(255, 255, 0, " + ratio / 2 + ")";
+		context.fill();
+
+		context.arc(positions[i].x + 60, positions[i].y - 15, ratio / 1.4, 0, 2 * Math.PI, true);
+		context.arc(positions[i].x + 60, positions[i].y - 10, ratio / 1.3, 0, 2 * Math.PI, true);
+		context.arc(positions[i].x + 60, positions[i].y + 10, ratio / 1.3, 0, 2 * Math.PI, true);
+		context.arc(positions[i].x + 60, positions[i].y + 15, ratio / 1.4, 0, 2 * Math.PI, true);
+		context.fillStyle = "rgba(255, 106, 106, " + ratio / 2 + ")";
+		context.fill();
+
+		context.arc(positions[i].x + 60, positions[i].y - 20, ratio / 1.5, 0, 2 * Math.PI, true);
+		context.arc(positions[i].x + 60, positions[i].y + 20, ratio / 1.5, 0, 2 * Math.PI, true);
+		context.fillStyle = "rgba(240, 126, 20, " + ratio / 2 + ")";
+		context.fill();
+	}
+	
+	//glowing background of the main circle
 
 	context.beginPath();
-	context.arc(mouseX, mouseY, 15, 0, 2 * Math.PI, true); //draws a line following the mouse
-	context.fillStyle = "#FF6A6A";
+	context.arc(mouseX + 60, mouseY, 7, 0, 2 * Math.PI, true); //draws a line following the mouse
+	context.fillStyle = "rgba(255, 255, 0, 0.5)";
 	context.fill();
+
+	context.beginPath();
+	context.arc(mouseX + 60, mouseY, 11, 0, 2 * Math.PI, true); //draws a line following the mouse
+	context.fillStyle = "rgba(255, 106, 106, 0.3)";
+	context.fill();
+
+	context.beginPath();
+	context.arc(mouseX + 60, mouseY, 17 , 0, 2 * Math.PI, true); //draws a line following the mouse
+	context.fillStyle = "rgba(240, 126, 20, 0.2)";
+	context.fill();
+
+	context.beginPath();
+	context.arc(mouseX + 60, mouseY, 25, 0, 2 * Math.PI, true); //draws a line following the mouse
+	context.fillStyle = "rgba(240, 126, 20, 0.05)";
+	context.fill();
+
+	//main circle
+	context.beginPath();
+	context.arc(mouseX + 60, mouseY, 5, 0, 2 * Math.PI, true); //draws a line following the mouse
+	context.fillStyle = "rgba(255, 255,255, 100)";
+	context.fill();	
 
 	storeLastPosition(mouseX, mouseY);
 
 	requestAnimationFrame(update);
 }
 
-var motionTrailLength = 100;
+var motionTrailLength = 125;
 var positions = [];
 
 function storeLastPosition(xPos, yPos) {
@@ -52,3 +98,4 @@ function storeLastPosition(xPos, yPos) {
 }
 
 update(); //the requestAnimationFrame callback calls an update function for the frames
+
